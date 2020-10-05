16dd178c7e726ee99c3238680bce47923c208aa4
Djuan Ellis <djuan.ellis@gmail.com>
2020-10-05 19:58:13 -0400

    add shipping calc

--- JAVA/BubbleSort.java
+++ JAVA/BubbleSort.java
@@ -1,37 +1,37 @@
-package arrays;
-
-public class BubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-             
-		
-		int a[] =  {2, 4, -1, -8, 6, 8, -12};
-		int n = a.length;
-		
-		boolean sorted = true;
-		
-		for(int i = 0; i<n-1; i++ ) {
-			for(int j = 0; j<n-1-i; j++) {
-				if(a[j+1] < a[j]) {
-					int temp = a[j+1];
-					a[j+1] = a[j];
-					a[j]  = temp;
-					
-					sorted = false;
+// Java program for implementation of Bubble Sort
+class BubbleSort
+{
+	void bubbleSort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n-1; i++)
+			for (int j = 0; j < n-i-1; j++)
+				if (arr[j] > arr[j+1])
+				{
+					// swap temp and arr[i]
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
-			}
-			
-			if(sorted) break;
-			
-		}
-		
-		for(int item: a) {
-			System.out.print(item+ " ");
-		}
-		
-             
-             
 	}
 
+	/* Prints the array */
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method to test above
+	public static void main(String args[])
+	{
+		BubbleSort ob = new BubbleSort();
+		int arr[] = {64, 34, 25, 12, 22, 11, 90};
+		ob.bubbleSort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
 }
+/* This code is contributed by Rajat Mishra */
