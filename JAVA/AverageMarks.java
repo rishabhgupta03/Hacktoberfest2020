86f5547a02aa2a8307d4243bde7a99b93d50e6c0
JaxonTeller <72128330+JaxonTeller@users.noreply.github.com>
2020-10-08 19:01:51 +0530

    Use Functional Style Java Programming
    Make use of Streams and Lambdas to achieve functional programming

--- JAVA/AverageMarks.java
+++ JAVA/AverageMarks.java
@@ -5,31 +5,12 @@ import java.util.Scanner;
 public class AverageMarks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         Scanner sc = new Scanner(System.in);
-         
-         System.out.println("Enter the number of students : ");
-         
-         int n = sc.nextInt();
-         
-         int marks[] = new int[n];
-         
-         System.out.println("Enter the marks now");
-         
-         for(int i = 0; i<n; i++) {
-        	 marks[i] = sc.nextInt();
-        	 
-         }
-         
-         int avg = 0;
-         
-         for(int i =0; i<n; i++) {
-        	 avg += marks[i];
-        	 } 
-           
-         avg /= n;
-        	 System.out.println("The average marks are  " + avg);
-         }
-
+	 Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of subjects : ");
+        int n = sc.nextInt();
+        int marks[] = new int[n];
+        System.out.println("Enter the marks now");
+        IntStream.range(0,n).forEach(mark->{ marks[mark] = sc.nextInt();});
+        System.out.println("The average marks are  " + Arrays.stream(marks).average().getAsDouble());
 
 }
