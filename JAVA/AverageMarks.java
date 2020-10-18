662363c33f7e456ca65849385bc977489c4d67ad
Deepanshu761 <72152421+Deepanshu761@users.noreply.github.com>
2020-10-18 13:02:58 +0530

    Update AverageMarks.java
    Minor Changes

--- JAVA/AverageMarks.java
+++ JAVA/AverageMarks.java
@@ -6,11 +6,12 @@ public class AverageMarks {
 
 	public static void main(String[] args) {
 	 Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of subjects : ");
+        System.out.println("Enter the number of subjects : \n");
         int n = sc.nextInt();
         int marks[] = new int[n];
         System.out.println("Enter the marks now");
         IntStream.range(0,n).forEach(mark->{ marks[mark] = sc.nextInt();});
         System.out.println("The average marks are  " + Arrays.stream(marks).average().getAsDouble());
 
-}
+} 
+} 
